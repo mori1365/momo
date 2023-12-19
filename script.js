3653d1368414d06f047c3c9741fbf0edd6e5f80d
mori1365 <105076694+mori1365@users.noreply.github.com>
2023-12-19 20:28:45 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const header = document.querySelector('header');
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", this.scrollY > 140)
+})
+let menu = document.getElementById('menu');
+let x = document.getElementById('x');
+let nav = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    nav.classList.toggle('open');
+}
+window.onscroll = () => {
+    menu.classList.remove("bx-x");
+    nav.classList.remove('open');
+}
